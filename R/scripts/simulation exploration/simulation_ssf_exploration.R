c962530ed3f7e150fdc72442d701b84ca04c4f0f
lcwat <lcwatson012@gmail.com>
2025-11-19 14:17:43 -0600

    a little more work with ssf, should move on to trying fit with bayesian

--- R/scripts/simulation exploration/simulation_ssf_exploration.R
+++ R/scripts/simulation exploration/simulation_ssf_exploration.R
@@ -27,12 +27,6 @@ nn_runs_expanded <- read_csv(
 
 glimpse(nn_runs_expanded)
 
-# inv dist appears to be gamma distributed, which makes sense since most options
-# are far away
-# cos ta is strange and has been described before by Fieberg 2021 to follow a 
-# vonmises circular distribution
-# cluster/neighbor values also appear strange 
-
 # create new step id variable for strata
 steps <- nn_runs_expanded |> 
   group_by(forager, collection_num) |> 
@@ -51,64 +45,101 @@ nn_runs_expanded <- nn_runs_expanded |>
 nn_runs_expanded <- nn_runs_expanded |> 
   arrange(step_id)
 
-#
-
-# model -------------------------------------------------------------------
-
 # drop nas
 nn_runs_expanded <- nn_runs_expanded |> 
   drop_na()
 
-# turn into factors
+# enumerate forager 1 through num(foragers)
 nn_runs_expanded$forager <- as.numeric(as.factor(nn_runs_expanded$forager))
+
+# center vars
+nn_runs_expanded <- nn_runs_expanded |> 
+  mutate(
+    c_distance = distance - mean(distance)
+  )
+
+#
+
+# model -------------------------------------------------------------------
 
 # variable names
 names(nn_runs_expanded)
 
+# may be inherent correlations between covariates based on used/unused
+nn_runs_expanded |> 
+  filter(used == 1) |> 
+  select(distance, cos_ta, neighbor_value, point_value) |> 
+  as.matrix() |> 
+  cor() |> 
+  round(digits = 2)
+
+# decent correlations between some covariates, particularly distance to turn angles
+# for used resources
+# for unused resources, decent correlation between distance and cluster value
+# which could complicate model's ability to detect pure strategies and introduce
+# essential collinearity 
+
 # glmmTMB fitting procedure
 # specify model with large fixed value for random effect variance (theta) of 
 # collection number (step)
-ssf_fit <- glmmTMB(
-  used ~ -1 + c_inv_dist * c_point_val + (1 | collection_num) + 
-    (0 + c_inv_dist * c_point_val | forager), 
+ssf_pois_fit <- glmmTMB(
+  used ~ -1 + distance + (1 | step_id) + 
+    (0 + distance | forager), 
   family = poisson, data = nn_runs_expanded, 
-  map = list(theta = factor(c(NA, 1:6))), # specify fixed re variance for col num
-  start = list(theta = c(log(1e3), 0, 0, 0, 0, 0, 0))
+  map = list(theta = factor(c(NA, 1))), # don't estimate step id variance
+  start = list(theta = c(log(1e3), 0)) # fix to 1e6
 )
 
-# singular fit
+# 120 re params for a full factorial! almost certainly a non-definite/Hessian and/or
+# singluar fit. bayesian has to be the way to go here
 
-# view results
-summary(ssf_fit)
+# ssf_logit <- glmer(
+#   used ~ 1 + c_inv_dist + (1 | step_id) + (c_inv_dist | forager), 
+#   family = binomial(), # standard logistic 
+#   data = nn_runs_expanded
+# )
+# 
+# # view results
+# summary(ssf_logit)
+summary(ssf_pois_fit)
 
 # can correctly identify that ta is not being used! but will say there is a 
 # significant interaction 
 
-confint(ssf_fit)
+confint(ssf_pois_fit)
 
 # seems to properly capture the differences between foragers, returning a rand
 # effect variance of about .3
 
 # values to sample
-inv_values <- seq(
-  min(nn_runs_expanded$inv_dist), max(nn_runs_expanded$inv_dist), length.out = 30
+dist_values <- seq(
+  min(nn_runs_expanded$distance), 
+  max(nn_runs_expanded$distance), 
+  length.out = 30
 )
-ta_values <- seq(
-  min(nn_runs_expanded$cos_ta), max(nn_runs_expanded$cos_ta), length.out = 30
-)
+# point_values <- unique(nn_runs_expanded$c_point_val)
 
-toplot <- ssf_fit |> 
-  emmeans(
-    ~inv_dist, 
-    at = list(inv_dist = inv_values), 
-    regrid = 'response'
-  ) |> 
-  as_tibble()
+toplot <- ssf_pois_fit |> 
+  predictions(
+    variables = 'distance'
+  )
 
 toplot |> 
-  ggplot(aes(x = inv_dist, y = rate)) +
+  mutate(
+    dist = c_distance + mean(nn_runs_expanded$distance), 
+    point = glue::glue('{c_point_val + mean(nn_runs_expanded$point_value)} points')
+  ) |> 
+  ggplot(aes(x = dist, y = emmean)) +
   
-  geom_line(color = '#fe1289') +
+  geom_line(aes(color = as.factor(point))) +
+  
+  geom_ribbon(
+    aes(ymin = asymp.LCL, ymax = asymp.UCL, fill = as.factor(point)), 
+    alpha = .2
+  ) +
+  
+  scale_color_viridis_d('', option = 'rocket', end = .8) +
+  scale_fill_viridis_d('', option = 'rocket', end = .8) +
   
   theme_bw()
 
@@ -118,7 +149,8 @@ toplot |>
 # b_ = N(0, 1)
 
 # estimate full factorial with maximal re structure
-# c_inv_dist * c_cos_ta * c_point_val * c_clst + (c_inv_dist * c_cos_ta * c_point_val * c_clst | id)
+# c_inv_dist * c_cos_ta * c_point_val * c_clst + 
+# (c_inv_dist * c_cos_ta * c_point_val * c_clst | id)
 
 # item re, if using level need to add that, otherwise just need collection_num 
 # as our stratum and fix the re variance to 10^6 as per Muff et al. (2019) to 
@@ -143,7 +175,7 @@ get_prior(
 my_priors <- c(
   set_prior('normal(0, 1)', class = 'b'),
   set_prior('exponential(1)', class = 'sd', group = 'id'),
-  set_prior('constant(1e-6)', class = 'sd', group = 'collection_num')
+  set_prior('normal(0, 1000000)', class = 'sd', group = 'step_id')
 )
 
 # run model
