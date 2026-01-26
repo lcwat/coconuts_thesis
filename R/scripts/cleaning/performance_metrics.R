1d74c3fe5b030c5b512279b557694795761585fa
lcwat <lcwatson012@gmail.com>
2026-01-26 12:31:46 -0600

    starting looking at the initial sona data

---
+++ R/scripts/cleaning/performance_metrics.R
@@ -0,0 +1,21 @@
+# coconut thesis
+# Spring 2026
+# Luke Watson
+
+# this script creates the performance metrics file from the pulled data
+
+# load libraries -------------------------------------------------------------
+
+library(tidyverse)
+
+# load data ------------------------------------------------------------------
+
+# load in datasets
+forage_data <- read_csv('data/clean_datasets/Jan_26_2026_forage_data.csv')
+location_data <- read_csv('data/clean_datasets/Jan_26_2026_location_data.csv')
+
+# source functions --------------------------------------------------------
+
+
+
+
---
+++ R/scripts/data_exploration/initial_data_exploration.R
@@ -0,0 +1,9 @@
+# coconut thesis
+# Spring 2026
+# Luke Watson
+
+# this script is a starting point to look at the data pulled from the database
+
+# load libraries -------------------------------------------------------------
+# load data ------------------------------------------------------------------
+
