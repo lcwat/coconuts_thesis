c8bb956a664a2de24a212879eec1cd2b56bc36e1
lcwat <lcwatson012@gmail.com>
2026-02-17 12:02:43 -0600

    added new round of data, collection almost done 93 pp, added credit for 1 participant

--- R/scripts/data_exploration/participant_credit.R
+++ R/scripts/data_exploration/participant_credit.R
@@ -8,6 +8,7 @@ library(tidyverse)
 
 # load data ---------------------------------------------------------------
 
+# GO INTO PARTICIPANT LIST CSV AND DELETE 'PARTICIPANTS' ON FIRST LINE
 # add roster download to participant_roster folder and read in file here
 participants <- read_csv('data/participant_roster/roster_download_1679.csv')
 
@@ -16,7 +17,7 @@ participants <- participants |>
   select(survey_id, grantor, credit_type, show_credit)
 
 # make sure to destroy and write anonymized file
-write_csv(participants, 'data/participant_roster/Feb_6_2026_credit_roster.csv')
+write_csv(participants, 'data/participant_roster/Feb_17_2026_credit_roster.csv')
 
 # read in forage data
 forage_data <- read_csv('data/clean_datasets/Feb_5_2026_forage_data.csv')
@@ -34,3 +35,7 @@ levels_completed <- forage_data |>
 # rectify with participants
 participants <- participants |> 
   left_join(levels_completed, by = join_by(survey_id == subject))
+
+# see who may need credit grant
+participants |> 
+  filter((show_credit < .5 & level_count >= 10) | !is.na(grantor))
